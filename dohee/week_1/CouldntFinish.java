d6b6a82a284b4c8fabea576e19e9372c0c0f69e0
doohee94 <doohee94@naver.com>
2020-10-27 22:05:13 +0900

    완주하지 못한 선수 코드 수정

--- dohee/week_1/CouldntFinish.java
+++ dohee/week_1/CouldntFinish.java
@@ -8,44 +8,27 @@ public class CouldntFinish {
     public static String solution(String[] participant, String[] completion) {
         String answer = "";
 
+        Map<String, Integer> participantMap = getAthletesMaps(participant);
         Map<String, Integer> completionMap = getAthletesMaps(completion);
 
-        for (String part : participant) {
-            if(!completionMap.containsKey(part)){
-                answer = part;
+        for (Map.Entry<String, Integer> partMap : participantMap.entrySet()) {
+            if(!completionMap.containsKey(partMap.getKey())){
+                answer = partMap.getKey();
                 break;
             }
-            
+
+            if(!partMap.getValue().equals( completionMap.get(partMap.getKey()))){
+                answer = partMap.getKey();
+                break;
+            }
+
         }
 
 
         return answer;
     }
-//
-//    public static String solution(String[] participant, String[] completion) {
-//        String answer = "";
-//
-//        Map<String, Integer> participantMap = getAthletesMaps(participant);
-//        Map<String, Integer> completionMap = getAthletesMaps(completion);
-//
-//        for (Map.Entry<String, Integer> partMap : participantMap.entrySet()) {
-//            if(!completionMap.containsKey(partMap.getKey())){
-//                answer = partMap.getKey();
-//                break;
-//            }
-//
-//            if(!partMap.getValue().equals( completionMap.get(partMap.getKey()))){
-//                answer = partMap.getKey();
-//                break;
-//            }
-//
-//        }
-//
-//
-//        return answer;
-//    }
 
-    private static Map<String, Integer> getAthletesMaps(String[] athletes) {
+    private  Map<String, Integer> getAthletesMaps(String[] athletes) {
 
         Map<String, Integer> map = new HashMap<>();
 
@@ -61,7 +44,6 @@ public class CouldntFinish {
 
         return  map;
     }
-
 
     public static void main(String[] args) {
 
